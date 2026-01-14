9276b5e0dc5d646c4ec2a79cd525a41171d9c9a3
Tran Thien Phuc <tranthienphuc123456789@gmail.com>
2026-01-14 23:23:37 +0700

    Setup swagger and integrate it into main.ts to generate api docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,58 @@
+import { setupSwagger } from "@libs/swagger";
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { SampleModule } from "./sample/sample.module";
+
+export function swaggerCustomScript(endpoint: string, tagOrder?: string[]) {
+    return [
+        bootstrap.toString(),
+        `bootstrap("${endpoint}", ${JSON.stringify(tagOrder)})`,
+    ];
+}
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix("api");
+    app.enableCors();
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+
+    const configService = app.get(ConfigService);
+    const apiEndpoint = configService.get("SERVER_URL");
+
+    const config = new DocumentBuilder()
+        .addServer(apiEndpoint)
+        .setTitle("AniHub v1 API Docs")
+        .setVersion("1.0")
+        .build();
+
+    const { document, tags } = setupSwagger(app, config, {
+        include: [SampleModule],
+    });
+
+    SwaggerModule.setup("/docs", app, document, {
+        customSiteTitle: "Pingpong v1 API Docs",
+        jsonDocumentUrl: "docs-json",
+        yamlDocumentUrl: "docs-yaml",
+        customJsStr: swaggerCustomScript(apiEndpoint, tags),
+        useGlobalPrefix: true,
+    });
+
+    const nodeEnv = configService.get("NODE_ENV");
+
+    if (nodeEnv === "production") {
+        app.enableShutdownHooks();
+    }
+
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
