2cb9a7ca93acf75469d5ed834ef96f2b71da744c
Tnina2612 <tranthienphuc123456789@gmail.com>
2026-03-07 22:28:01 +0700

    Add models and fields involving Reaction

---
+++ server/src/message/response/message.response.ts
@@ -0,0 +1,50 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Expose, Type } from "class-transformer";
+import { AttachmentPayload } from "src/posts/dto";
+import { AuthorDto } from "src/users/dto";
+
+export class ReactionGroupDto {
+  icon: string;
+  count: number;
+  users: AuthorDto[];
+}
+
+export class MessageResponseDto {
+  @ApiProperty({
+    description: "Unique identifier of the message",
+    example: "550e8400-e29b-41d4-a716-446655440000",
+    type: String,
+  })
+  @Expose()
+  id: string;
+
+  @ApiProperty({
+    description: "Content of the message",
+    example: "Just shared an amazing photo from my vacation!",
+    type: String,
+  })
+  @Expose()
+  content?: string;
+
+  @ApiProperty({
+    description: "Array of media attachments for the message",
+  })
+  @Expose()
+  attachments: AttachmentPayload[];
+
+  replyTo?: {
+    id: string;
+    content: string;
+  }
+
+  @ApiProperty({
+    description: "Sender of the message",
+    type: () => AuthorDto,
+  })
+  @Expose()
+  @Type(() => AuthorDto)
+  sender: AuthorDto;
+
+  @Expose()
+  reactions: [ReactionGroupDto];
+}
